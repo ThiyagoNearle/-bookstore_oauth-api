f441eed3fd189270f1119bb035994a4c9f9f2b73
arunkumar_hockey <arunkumar.hockey@gmail.com>
2021-12-31 19:04:32 +0530

    commit

--- src/domain/access_token/access_token.go
+++ src/domain/access_token/access_token.go
@@ -20,15 +20,17 @@ type AccessToken struct {
 
 func GetNewAccessToken() AccessToken {
 	return AccessToken{
-		Expires: time.Now().UTC().Add(expirationTime * time.Hour).Unix(), // unix is the time stamp of the current time
+		Expires: time.Now().UTC().Add(expirationTime * time.Hour).Unix(), // unix is the time stamp of the current time, it gives result in seconds
 	}
 }
 
 func (at AccessToken) IsExpired() bool {
 	now := time.Now().UTC()
-	expirationTime := time.Unix(at.Expires, 0)
+	expirationTime := time.Unix(at.Expires, 0) // convert that seconds into datetime
 
-	return expirationTime.Before(now)
+	return expirationTime.Before(now) // it means [ expirationTime < now  ]  if the condition is ok, it gives true ohtherwise false
+	// Before reports whether the time  t (expirationTime) is before u (now)
+
 	// WE CAN WRITE THE ABOVE 3 LINES OF CODE IN THIS BELOW 1 LINE
 
 	// return time.Unix(at.Expires, 0).Before(time.Now()).UTC())
